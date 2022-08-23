0ee83054cbee3217518397e26911fa2a3e182fa5
ithersta <schilyaev.vlad@gmail.com>
2022-08-23 21:26:18 +0400

    Add role stubs

--- src/main/kotlin/ru/spbstu/preaccelerator/telegram/StateMachine.kt
+++ src/main/kotlin/ru/spbstu/preaccelerator/telegram/StateMachine.kt
@@ -4,6 +4,7 @@ import com.ithersta.tgbotapi.fsm.builders.stateMachine
 import com.ithersta.tgbotapi.fsm.repository.StateRepository
 import dev.inmo.tgbotapi.types.UserId
 import ru.spbstu.preaccelerator.domain.entities.Role
+import ru.spbstu.preaccelerator.domain.entities.Role.*
 import ru.spbstu.preaccelerator.telegram.entities.state.DialogState
 import ru.spbstu.preaccelerator.telegram.flows.cancelCommand
 import ru.spbstu.preaccelerator.telegram.flows.fallback
@@ -12,12 +13,20 @@ import ru.spbstu.preaccelerator.telegram.flows.startFlow
 fun createStateMachine(
     stateRepository: StateRepository<UserId, DialogState>
 ) = stateMachine<Role, DialogState>(
-    getRole = { null },
-    stateRepository = stateRepository
+    getRole = { null }, stateRepository
 ) {
     includeHelp()
     withoutRole {
         startFlow()
+    }
+    role(Curator) {
+
+    }
+    role(Tracker) {
+
+    }
+    role(Member) {
+
     }
     anyRole {
         anyState {
