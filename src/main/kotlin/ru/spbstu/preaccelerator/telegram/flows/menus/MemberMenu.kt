b4bab9ff32cded4c8b20f20ddfa536fa15aa2ebd
Владислав Шиляев <schilyaev.vlad@gmail.com>
2022-09-01 16:49:12 +0300

    Add menus for all roles (#11)
    * Add menus for all roles
    
    * Add emoji to back button
    
    * Display teams on tracker's /who

---
+++ src/main/kotlin/ru/spbstu/preaccelerator/telegram/flows/menus/MemberMenu.kt
@@ -0,0 +1,18 @@
+package ru.spbstu.preaccelerator.telegram.flows.menus
+
+import com.ithersta.tgbotapi.fsm.builders.RoleFilterBuilder
+import com.ithersta.tgbotapi.menu.menu
+import dev.inmo.tgbotapi.types.UserId
+import ru.spbstu.preaccelerator.domain.entities.user.Member
+import ru.spbstu.preaccelerator.domain.entities.user.PreacceleratorUser
+import ru.spbstu.preaccelerator.telegram.entities.state.DialogState
+import ru.spbstu.preaccelerator.telegram.entities.state.EmptyState
+import ru.spbstu.preaccelerator.telegram.entities.state.NotImplementedState
+import ru.spbstu.preaccelerator.telegram.resources.strings.MenuStrings
+
+fun RoleFilterBuilder<DialogState, PreacceleratorUser, Member, UserId>.memberMenu() {
+    menu(MenuStrings.Member.Message, EmptyState) {
+        button(MenuStrings.Member.SelectModule, NotImplementedState)
+        button(MenuStrings.Member.GetRating, NotImplementedState)
+    }
+}
