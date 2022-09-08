f4e5d3f127a886417cb4794bd245282e6659e294
Gleb121region <71015316+Gleb121region@users.noreply.github.com>
2022-09-08 23:11:11 +0300

    implemented methods for all classes Protocol…

--- src/main/kotlin/ru/spbstu/preaccelerator/domain/repository/ProtocolRepository.kt
+++ src/main/kotlin/ru/spbstu/preaccelerator/domain/repository/ProtocolRepository.kt
@@ -1,3 +1,9 @@
 package ru.spbstu.preaccelerator.domain.repository
 
-interface ProtocolRepository
+import ru.spbstu.preaccelerator.domain.entities.Protocol
+import ru.spbstu.preaccelerator.domain.entities.Team
+
+interface ProtocolRepository {
+    fun get(teamId: Team.Id): Protocol
+    fun add(teamId: Team.Id, url: String)
+}
