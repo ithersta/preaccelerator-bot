ec38cbd1907a43775acabeaae8f176c00fe9819a
ithersta <schilyaev.vlad@gmail.com>
2022-09-08 20:32:48 +0300

    Add moduleNumber to Meeting

--- src/main/kotlin/ru/spbstu/preaccelerator/domain/entities/Meeting.kt
+++ src/main/kotlin/ru/spbstu/preaccelerator/domain/entities/Meeting.kt
@@ -1,10 +1,12 @@
 package ru.spbstu.preaccelerator.domain.entities
 
+import ru.spbstu.preaccelerator.domain.entities.module.Module
 import java.time.OffsetDateTime
 
 class Meeting(
     val id: Id,
     val teamId: Team.Id,
+    val moduleNumber: Module.Number,
     val timestamp: OffsetDateTime,
     val url: String
 ) {
