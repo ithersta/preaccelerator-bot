3b0d52e562dc4237404fb996e96c2d83b009832e
ithersta <schilyaev.vlad@gmail.com>
2022-09-09 12:33:42 +0300

    Use mutex::withLock function

--- src/main/kotlin/ru/spbstu/preaccelerator/telegram/notifications/MassSendLimiter.kt
+++ src/main/kotlin/ru/spbstu/preaccelerator/telegram/notifications/MassSendLimiter.kt
@@ -2,6 +2,7 @@ package ru.spbstu.preaccelerator.telegram.notifications
 
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.sync.Mutex
+import kotlinx.coroutines.sync.withLock
 import org.koin.core.annotation.Single
 import kotlin.time.Duration.Companion.seconds
 
@@ -9,9 +10,7 @@ import kotlin.time.Duration.Companion.seconds
 class MassSendLimiter {
     private val mutex = Mutex()
 
-    suspend fun wait() {
-        mutex.lock()
+    suspend fun wait() = mutex.withLock {
         delay(0.2.seconds)
-        mutex.unlock()
     }
 }
