4375d363b715d8496714ba5ebb548b06ffeb2d81
Владислав Шиляев <schilyaev.vlad@gmail.com>
2022-09-30 21:36:59 +0300

    Use different schedulers for different jobs (#86)
    * Use different schedulers for different jobs
    
    * Temporarily use snapshot version of SQLDelight

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,8 @@
 rootProject.name = "preaccelerator-bot"
+
+pluginManagement {
+    repositories {
+        mavenCentral()
+        maven { url = uri("https://oss.sonatype.org/content/repositories/snapshots/") }
+    }
+}
